db91726774d8b0609b1a8b737cc950378348b8a2
Yoel Ribero <yoelribero9@gmail.com>
2020-09-02 22:06:41 -0300

    Agrega dos nuevas imágenes de background hero y comienzo de desarrollo de API

---
+++ build/precache-manifest.f4d9c72a8dadb6f7e408fa70ea012158.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182872171ac823c11aba2ec30e1a6fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8013a44ca377d46b4a8",
+    "url": "/static/css/main.b7b0c44b.chunk.css"
+  },
+  {
+    "revision": "5c28bc0feee3a38a8ac1",
+    "url": "/static/js/2.abf547d9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.abf547d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8013a44ca377d46b4a8",
+    "url": "/static/js/main.785c153a.chunk.js"
+  },
+  {
+    "revision": "08129d5b7f70bc71d88a",
+    "url": "/static/js/runtime-main.16910e82.js"
+  },
+  {
+    "revision": "b738fb2ea90ef1d2c4ee25fca7be1666",
+    "url": "/static/media/construction.b738fb2e.jpg"
+  },
+  {
+    "revision": "2e635abc2d33815a020f8086cfcd89c4",
+    "url": "/static/media/gastronomy.2e635abc.jpg"
+  },
+  {
+    "revision": "92854cc0af1121120ccd2e5996371bc3",
+    "url": "/static/media/hero.92854cc0.png"
+  },
+  {
+    "revision": "a25829ced99367b82128d286d419dc5a",
+    "url": "/static/media/photography.a25829ce.jpg"
+  },
+  {
+    "revision": "6f02bd9671760b4b59b895e3d885a44d",
+    "url": "/static/media/profile-avatar.6f02bd96.jpg"
+  },
+  {
+    "revision": "20d27b82e70ac087992ea669bbe3c12b",
+    "url": "/static/media/programming.20d27b82.jpg"
+  },
+  {
+    "revision": "e30438d4d3cac7c78aeb408cfcc8c867",
+    "url": "/static/media/singer.e30438d4.jpg"
+  }
+]);
